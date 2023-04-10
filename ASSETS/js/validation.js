d1c67326aabbc91356815a4dbc8e7ef1f8594c5b
harikarthik13 <harikarthik1301@gmail.com>
2023-04-10 23:56:19 +0530

    finished

--- ASSETS/js/validation.js
+++ ASSETS/js/validation.js
@@ -5,29 +5,31 @@ $(document).ready(function(){
                 required:true,
                 pattern:/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$#])[A-Za-z\d@$#]{7,15}$/,
             },
-            mbl:{
-                required:true,
-                pattern:/^[0-9]{3}-[0-9]{4}-[0-9]{4}$/,
-            },
             mail:{
                 required:true,
                 email:true,
                 pattern:/^([a-zA-Z0-9_])+@cgvak.com$/,
             },
+            mbl:{
+                required:true,
+                pattern:/^[0-9]{3}-[0-9]{4}-[0-9]{4}$/,
+            },
+        
         },
         messages:{
             pass:{
-                required:"Enter your Name",
-                pattern:"Enter the valid Name",
+                required:"Enter User Name",
+                pattern:"Enter the Valid User Name",
+            },
+            mail:{
+                required:"Enter your E-mail ID",
+                email:"Enter Valid E-mail ID",
             },
             mbl:{
                 required:"Enter your Phone number",
                 pattern:"Enter the valid number",
             },
-            mail:{
-                required:"enter your email",
-                email:"enter valid email",
-            },
+         
         },
     });
 });
